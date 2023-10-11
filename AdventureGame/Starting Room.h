3c2592439152687e4ff88f673043f7c6ee422c6b
Nicholas Torres <n.torres1@student.fdu.edu>
2023-10-11 17:11:01 -0400

    Minor text fixes. MBUILD18

--- AdventureGame/Starting Room.h
+++ AdventureGame/Starting Room.h
@@ -11,7 +11,12 @@ class StartingRoom : public Action
 protected:
 	virtual void printLocation()
 	{
-		// add code 
+		cout << "You are in a vibrant, yet desolate forest." << endl;
+		cout << "There seems to be no wildlife in sight." << endl;
+		cout << "There seems to be a shallow pond to the west, " << endl;
+		cout << "a deserted hut to the east, and more wilderness. " << endl;
+		cout << "south and north of your location." << endl;
+		cout << "Where would you like to go?" << endl;
 
 		Action::Move();
 	}
@@ -22,12 +27,13 @@ protected:
 		cout << "You ask yourself, how the hell did I get here and what is my purpose here? " << endl;
 		cout << "Well, the more you explore, the more you learn about not only where you are, but who you are. " << endl;
 		cout << "You start to find ripped up pieces of paper on the ground, and one piece says in bold ink: WANTED. " << endl;
-		cout << "But you cannot seem to make out who’s head has the price on it. " << endl;
+		cout << "But you cannot seem to make out whose head has the price on it. " << endl;
 		cout << "The deeper you go into the game, you start to understand you are the one wanted? But what for? " << endl;
 		cout << "Well, you and the princess of the mighty kingdom used to be great lovers, " << endl;
 		cout << "but then something happened and now your reputation and life has been flipped upside down. " << endl;
 		cout << "You now have to evade angry villagers and when you encounter them, you must fight for survival. " << endl;
-		cout << "Your quest is to get into castle and find out what happened to your great relationship with the princess and try to win her heart back." << endl;
+		cout << "Your quest is to get into castle and find out what happened to your great relationship with the princess ";
+		cout << "and try to win her heart back." << endl << endl;
 	}
 public:
 	StartingRoom()
