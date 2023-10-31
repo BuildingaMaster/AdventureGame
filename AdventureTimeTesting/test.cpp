ad8de69f2cb77b2959a6e81cbcb0695cfdb83995
jfduedu <144645190+jfduedu@users.noreply.github.com>
2023-10-31 01:08:15 -0400

    MBUILD-59 Small Restructure and re-write test

--- AdventureTimeTesting/test.cpp
+++ AdventureTimeTesting/test.cpp
@@ -11,9 +11,9 @@
 namespace
 {
     Inventory userInventory;
-    Location startingRoom;
-    PlayerActions playerAct;
-    ContextParser CP(&startingRoom, &userInventory, &playerAct);
+    Location *startingRoom = new Location();
+    PlayerActions playeract;
+    ContextParser CP(&userInventory, &playeract);
 
     TEST(ItemTest, Init)
     {
@@ -26,21 +26,21 @@ namespace
     {
         string command;
         command = "";
-        EXPECT_FALSE(CP.interpretCommand(command));
+        EXPECT_FALSE(CP.interpretCommand(startingRoom,command));
     }
 
     TEST(ContextTest, TooShort)
     {
         string command;
         command = "lol";
-        EXPECT_FALSE(CP.interpretCommand(command));
+        EXPECT_FALSE(CP.interpretCommand(startingRoom,command));
     }
 
     TEST(ContextTest, WrongShortCommand)
     {
         string command;
         command = "sume apple";
-        EXPECT_FALSE(CP.interpretCommand(command));
+        EXPECT_FALSE(CP.interpretCommand(startingRoom,command));
     }
 
     TEST(ContextTest, OutOfOrderTest)
@@ -48,26 +48,26 @@ namespace
         string command;
         command = "consume apple";
         testing::internal::CaptureStdout();
-        EXPECT_FALSE(CP.interpretCommand(command));
+        EXPECT_FALSE(CP.interpretCommand(startingRoom,command));
         string output = testing::internal::GetCapturedStdout();
         EXPECT_EQ(output, "\nYou don't have an apple.\n");
 
 
         command = "pick apple";
         testing::internal::CaptureStdout();
-        EXPECT_TRUE(CP.interpretCommand(command));
+        EXPECT_TRUE(CP.interpretCommand(startingRoom,command));
         output = testing::internal::GetCapturedStdout();
         EXPECT_EQ(output, "\nYou pick an apple from the tree.\n");
 
         command = "pick apple";
         testing::internal::CaptureStdout();
-        EXPECT_FALSE(CP.interpretCommand(command));
+        EXPECT_FALSE(CP.interpretCommand(startingRoom,command));
         output = testing::internal::GetCapturedStdout();
         EXPECT_EQ(output, "\nThere is no apple for you to pick!\n");
 
         command = "eat apple";
         testing::internal::CaptureStdout();
-        EXPECT_TRUE(CP.interpretCommand(command));
+        EXPECT_TRUE(CP.interpretCommand(startingRoom,command));
         output = testing::internal::GetCapturedStdout();
         EXPECT_EQ(output, "\nYou eat the apple!\n");
     }
@@ -75,8 +75,8 @@ namespace
     int main(int argc, char** argv)
     {
 
-        startingRoom.setDescription("You are in a vibrant, yet desolate forest. \nThere seems to be no wildlife in sight, although a nearby apple tree seems to be within reach. \nTo the west is a shallow pond, \na deserted hut to the east, and more wilderness \nsouth and north of your location.");
-        startingRoom.printLocation();
+        startingRoom->setDescription("You are in a vibrant, yet desolate forest. \nThere seems to be no wildlife in sight, although a nearby apple tree seems to be within reach. \nTo the west is a shallow pond, \na deserted hut to the east, and more wilderness \nsouth and north of your location.");
+        startingRoom->printLocation();
         testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
     }
