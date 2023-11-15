96f5ae5d5fa56b90b6925c92867ace28bc3e2db1
Juan23071 <j.gonzalez437@student.fdu.edu>
2023-11-15 13:39:52 -0500

    end of life will tell user "game over"

--- AdventureGame/PlayerActions.cpp
+++ AdventureGame/PlayerActions.cpp
@@ -36,7 +36,7 @@ bool PlayerActions::processCommand(vector<string> args)
     {
         if (healthMGR.removeHP(1)) // The player is dead.
         {
-            PrintDisplay::custom_cout << "\nFor some reason, you hit yourself and slapped yourself to death. YOU WIN!!!\n\nJust Joking:";
+            PrintDisplay::custom_cout << "GAME OVER! ";
             PrintDisplay::flush();
         }
         else
